5f6fe1d14236efdb7652728b1106ecfe89fcbf37
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2026-05-08 16:01:00 -0300

    libs

--- back-end/Repositorys/obj/Debug/net8.0/Repositorys.AssemblyInfo.cs
+++ back-end/Repositorys/obj/Debug/net8.0/Repositorys.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositorys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c236e59dd07ba4383b436d9396e1be86f1584b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositorys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositorys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
