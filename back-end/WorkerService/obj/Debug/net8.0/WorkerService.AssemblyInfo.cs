7828e323375e910b35eb82f20c953a0d4cfce147
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2026-05-09 10:51:54 -0300

    libs

--- back-end/WorkerService/obj/Debug/net8.0/WorkerService.AssemblyInfo.cs
+++ back-end/WorkerService/obj/Debug/net8.0/WorkerService.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("dotnet-WorkerService-6552bf8a-f68f-43a4-8c0d-8bfc7e08c708")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb04394f598ff68c2d6db4cbad23dc94c85fac5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6100ef70e711dd059e6d39f2dff1c20243160d61")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkerService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
